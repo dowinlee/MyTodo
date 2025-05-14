dbc9bf2959b7b388256405962119f973b9ca3d1a
dowinlee <dowinlee@163.com>
2025-05-14 23:48:27 +0800

    Initial Commit

---
+++ MyTodoTests/MyTodoTests.swift
@@ -0,0 +1,17 @@
+//
+//  MyTodoTests.swift
+//  MyTodoTests
+//
+//  Created by dowinlee on 5/14/25.
+//
+
+import Testing
+@testable import MyTodo
+
+struct MyTodoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
